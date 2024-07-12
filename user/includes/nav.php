fe667a26b745fbc1b005cd42b49038ebe6df43e1
Atharva Karval <113116636+ironman2024@users.noreply.github.com>
2024-07-12 18:45:01 +0530

    Add files via upload

---
+++ user/includes/nav.php
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary-color">
+  <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <!-- Font Awesome CDN-->
+    <script src="https://kit.fontawesome.com/dd6d077d8c.js" crossorigin="anonymous"></script>
+  <a class="navbar-brand site-title" href="#"><i class="fas fa-heartbeat"></i> MedExpress</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li>
+        <button class="btn btn-light my-2 my-sm-0"><a href="../index.php">Home</a></button>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Product List
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href=".\user\babycare.php">Baby Care</a>
+          <a class="dropdown-item" href=".\user\covid-19essentials.php">COVID-19 Essentials</a>
+          <a class="dropdown-item" href=".\user\dailycare.php">Daily Care</a>
+          <a class="dropdown-item" href=".\user\DiabeticNeeds.php">Diabetic Needs</a>
+          <a class="dropdown-item" href=".\user\Health&Nutrition.php">Health & Nutrition</a>
+          <a class="dropdown-item" href=".\user\homeopathy.php">Homeopathy</a>
+        </div>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <button class="btn btn-light my-2 my-sm-0" type="submit"><a href="../login.php">LOGIN</a></button>
+    </form>
+  </div>
+</nav>
